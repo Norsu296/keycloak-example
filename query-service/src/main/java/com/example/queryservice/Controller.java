76eb9748af096e53c1f5ee6703ade2ede4d1a550
Jakub Brózda <brozdajakub@gmail.com>
2022-06-22 19:47:15 +0200

    initial

---
+++ query-service/src/main/java/com/example/queryservice/Controller.java
@@ -0,0 +1,15 @@
+package com.example.queryservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/query")
+public class Controller {
+
+    @GetMapping
+    String sayName() {
+        return "Query service";
+    }
+}
